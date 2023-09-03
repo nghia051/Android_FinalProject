4c85fe98719fc8ddbe4bfb75b750fd2f2fd9db60
Bui Nguyen Hoang <51512556+hoangteo0103@users.noreply.github.com>
2023-09-03 20:46:05 +0700

    Finisish Login UI and Firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:antap/login.dart';
-import 'package:antap/register.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:antap/screens/home_screen.dart';
+import 'package:antap/screens/login_screen.dart';
+import 'package:antap/screens/signup_screen.dart';
+import 'package:antap/screens/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-  await Hive.initFlutter();
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyLogin(),
-    routes: {
-      'register': (context) => MyRegister(),
-      'login': (context) => MyLogin(),
-    },
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: const TextTheme(
+        bodyMedium: TextStyle(
+          fontFamily: 'Ubuntu',
+        ),
+      )),
+      initialRoute: HomeScreen.id,
+      routes: {
+        HomeScreen.id: (context) => HomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        SignUpScreen.id: (context) => SignUpScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+      },
+    );
+  }
 }

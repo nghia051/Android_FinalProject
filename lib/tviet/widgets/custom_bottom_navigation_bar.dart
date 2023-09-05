65bbbf91e6700dc2f52b8950025a7f3805e26c47
Trần Việt <62978375+ttviet2805@users.noreply.github.com>
2023-09-05 21:29:18 +0700

    complete tiktok

---
+++ lib/tviet/widgets/custom_bottom_navigation_bar.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class CustomerBottomNavigationBar extends StatelessWidget {
+  const CustomerBottomNavigationBar(
+      {super.key, required this.selectedPageIndex, required this.onIconTap});
+  final int selectedPageIndex;
+  final Function onIconTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final barHeight = MediaQuery.of(context).size.height * 0.06;
+    final style = Theme.of(context)
+        .textTheme
+        .bodyLarge!
+        .copyWith(fontSize: 15, fontWeight: FontWeight.w600);
+
+    return BottomAppBar(
+      color: selectedPageIndex == 0 ? Colors.black : Colors.white,
+      child: Container(
+        height: barHeight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            _bottomBarNavItem(0, 'Home', style, Icons.home),
+            _bottomBarNavItem(1, 'Shop', style, Icons.store),
+            _addVideoNavItem(barHeight),
+            _bottomBarNavItem(3, 'Location', style, Icons.location_on),
+            _bottomBarNavItem(4, 'Profile', style, Icons.account_circle),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _bottomBarNavItem(
+      int index, String label, TextStyle textStyle, IconData iconData) {
+    bool isSelected = selectedPageIndex == index;
+    Color iconAndTextColor = isSelected ? Colors.black : Colors.grey;
+
+    if (isSelected && selectedPageIndex == 0) {
+      iconAndTextColor = Colors.white;
+    }
+
+    return GestureDetector(
+      onTap: () => {onIconTap(index)},
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            iconData,
+            color: iconAndTextColor,
+            size: 25,
+          ),
+          SizedBox(
+            height: 3,
+          ),
+          Text(label, style: textStyle.copyWith(color: iconAndTextColor)),
+        ],
+      ),
+    );
+  }
+
+  _addVideoNavItem(double height) {
+    return Container(
+      height: height - 15,
+      width: 48,
+      margin: EdgeInsets.only(bottom: 4),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [Colors.blueAccent, Colors.redAccent]),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Center(
+        child: Container(
+          width: 41,
+          height: height - 15,
+          decoration: BoxDecoration(
+            color: selectedPageIndex == 0 ? Colors.white : Colors.black,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Icon(Icons.add,
+              color: selectedPageIndex == 0 ? Colors.black : Colors.white),
+        ),
+      ),
+    );
+  }
+}

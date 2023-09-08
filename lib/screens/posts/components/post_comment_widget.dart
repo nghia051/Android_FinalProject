45ca2384db5420b4c92733b41c3a9b0c07c5505a
hoangnghiaviet <viethoangnghia@gmail.com>
2023-09-08 16:30:45 +0700

    update post screen

---
+++ lib/screens/posts/components/post_comment_widget.dart
@@ -0,0 +1,101 @@
+import 'package:antap/models/post.dart';
+import 'package:flutter/material.dart';
+
+class PostCommentWidget extends StatefulWidget {
+  final Post post;
+  const PostCommentWidget({required this.post, super.key});
+
+  @override
+  State<PostCommentWidget> createState() => _PostCommentWidgetState();
+}
+
+class _PostCommentWidgetState extends State<PostCommentWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      elevation: 0, // Tắt độ bóng của dialog
+      backgroundColor: Colors.black, 
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width, 
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Bình luận',
+              style: TextStyle(
+                color: Colors.white
+              )
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: widget.post.listComment.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 15),
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.account_circle_outlined,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(width: 10,),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.post.listComment[index].user,
+                                style: const TextStyle(
+                                  color: Colors.white
+                                )
+                              ),
+                              Text(
+                                widget.post.listComment[index].content,
+                                style: const TextStyle(
+                                  color: Colors.white
+                                )
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Container(
+              color: Colors.grey[800],
+              margin: const EdgeInsets.only(top: 5),
+              padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+              width: double.infinity,
+              child: const Row(
+                children: [
+                  Icon(
+                    Icons.account_circle_outlined,
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 10,),
+                  Expanded(
+                    child: TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: 'Viết bình luận...',
+                        hintStyle: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/posts/components/post_image_widget.dart
@@ -0,0 +1,76 @@
+import 'package:antap/models/post.dart';
+import 'package:flutter/material.dart';
+
+class PostImageWidget extends StatefulWidget {
+  final Post post;
+  int curImage = 0;
+
+  PostImageWidget({required this.post, super.key});
+
+  @override
+  State<PostImageWidget> createState() => _PostImageWidgetState();
+}
+
+class _PostImageWidgetState extends State<PostImageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center, // Đặt hình ảnh và nút chính giữa
+      children: [
+        Image.network(
+          widget.post.listImageUrl[widget.curImage],
+          height: 300,
+          fit: BoxFit.cover, // Đặt cách hiển thị hình ảnh
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              padding: EdgeInsets.zero,
+              icon: const Icon(
+                Icons.arrow_left,
+                size: 50,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                setState(() {
+                  widget.curImage = (widget.curImage - 1 + widget.post.listImageUrl.length) % widget.post.listImageUrl.length;
+                });
+              },
+            ),
+            IconButton(
+              padding: EdgeInsets.zero,
+              icon: const Icon(
+                Icons.arrow_right,
+                size: 50,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                setState(() {
+                  widget.curImage = (widget.curImage + 1) % widget.post.listImageUrl.length;
+                });
+              },
+            ),
+          ],
+        ),
+        Positioned( 
+          top: 10,
+          right: 10,
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(8, 5, 8, 5),
+            decoration: BoxDecoration(
+              color: Colors.black, // Đặt màu nền đen
+              borderRadius: BorderRadius.circular(10.0), // Độ bo góc
+            ),
+            child: Text(
+              "${widget.curImage + 1}/${widget.post.listImageUrl.length}",
+              style: const TextStyle(
+                color: Colors.white,
+              )
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
